22fcd8b24d5c63bb82b44e67c48d564573207390
susumu1031 <info.mycaddy@gmail.com>
2021-08-19 00:03:53 +0900

    メモアプリからのDB接続

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 require 'securerandom'
+require 'pg'
+require "erb"
+include ERB::Util
 
 helpers do
   def h(text)
@@ -10,9 +13,13 @@ helpers do
 end
 
 get '/memo' do
-  memos = Dir.glob('memo_data/*')
-  @hash = memos.map { |x| JSON.load(File.read(x)) }
-  erb :memo_top
+  connection = PG::connect(:host => "localhost", :user => "postgres", :dbname => "memoapp")
+  begin
+    @hash = connection.exec("SELECT * from memodata")
+    erb :memo_top
+  ensure
+    connection.close
+  end
 end
 
 get '/memo/new' do
@@ -22,45 +29,63 @@ end
 post '/memo_data' do
   @title = params[:title]
   @message = params[:message]
-  @id = params[:id]
-  @time = Time.now
-  hash = { 'id' => SecureRandom.uuid, 'title' => @title, 'message' => h(@message), 'time' => @time }
-  File.open("memo_data/#{hash['id']}.json", 'w') do |file|
-    JSON.dump(hash, file)
+  @id = SecureRandom.uuid
+  #@time = Time.now
+  connection = PG::connect(:host => "localhost", :user => "postgres", :dbname => "memoapp")
+  begin
+    connection.exec( "insert into memodata (id, title, message) values ('#{@id}', '#{@title}', '#{@message}')")
+  ensure
+    connection.close
   end
   redirect to('/memo')
 end
 
 get '/memo_data/:id' do
   @id = params[:id]
-  File.open("memo_data/#{@id}.json") do |file|
-    @hash = JSON.load(file)
+  connection = PG::connect(:host => "localhost", :user => "postgres", :dbname => "memoapp")
+  begin
+    @result = connection.exec("SELECT title, message from memodata WHERE id = '#{@id}'")
+    @hash = @result[0]
+    erb :reconfirm
+  ensure
+    connection.close
   end
-  erb :reconfirm
 end
 
 get '/memo_data/:id/edit' do
   @id = params[:id]
-  File.open("memo_data/#{@id}.json") do |file|
-    @hash = JSON.load(file)
+  connection = PG::connect(:host => "localhost", :user => "postgres", :dbname => "memoapp")
+  begin
+    @result = connection.exec("SELECT title, message from memodata WHERE id = '#{@id}'")
+    @hash = @result[0]
+    erb :edit
+  ensure
+    connection.close
   end
-  erb :edit
 end
 
 patch '/memo_data/:id' do
   @id = params[:id]
   @title = params[:title]
   @message = params[:message]
-  @time = Time.now
-  hash = { 'id' => @id, 'title' => @title, 'message' => h(@message), 'time' => @time }
-  File.open("memo_data/#{@id}.json", 'w') do |file|
-    JSON.dump(hash, file)
+  @id = params[:id]
+  connection = PG::connect(:host => "localhost", :user => "postgres", :dbname => "memoapp")
+  begin
+    result = connection.exec("UPDATE memodata set title = '#{@title}', message = '#{@message}' WHERE id = '#{@id}'")
+  ensure
+    connection.close
   end
   redirect to('memo')
 end
 
 delete '/memo_data/:id' do
   @id = params[:id]
-  File.delete("memo_data/#{@id}.json")
+  connection = PG::connect(:host => "localhost", :user => "postgres", :dbname => "memoapp")
+  begin
+    result = connection.exec("DELETE from memodata where id ='#{@id}'")
+    #File.delete("memo_data/#{@id}.json")
+  ensure
+    connection.close
+  end
   redirect to('/memo')
 end
